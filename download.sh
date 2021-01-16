44a9fa397324761a4cce87014dc655461945fbe6
Zaid Alyafeai <alyafey22@gmail.com>
2021-01-16 04:41:46 +0300

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,13 @@
+wget http://www.cs.toronto.edu/~rkiros/models/dictionary.txt
+wget http://www.cs.toronto.edu/~rkiros/models/utable.npy
+wget http://www.cs.toronto.edu/~rkiros/models/btable.npy
+wget http://www.cs.toronto.edu/~rkiros/models/uni_skip.npz
+wget http://www.cs.toronto.edu/~rkiros/models/uni_skip.npz.pkl
+wget http://www.cs.toronto.edu/~rkiros/models/bi_skip.npz
+wget http://www.cs.toronto.edu/~rkiros/models/bi_skip.npz.pkl
+
+mkdir Data/skipthoughts/
+cp *.npz Data/skipthoughts/
+cp *.pkl Data/skipthoughts/
+cp *.npy Data/skipthoughts/
+cp dictionary.txt Data/skipthoughts
